8f5577cdb498b02ea3d6a65397cca1efbbb9ebf9
SULTAN-SWE <sultans8477@gmail.com>
2026-04-15 08:38:00 +0300

    TODO 2 is done

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 
 // import dotenv and load environment variables from .env
+import dotenv from "dotenv";
 
+dotenv.config();
 
 import { connectDB } from "./db.js";
 import { Song } from "./models/song.model.js";
